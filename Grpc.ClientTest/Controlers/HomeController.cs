1bda1bc6c20842702e6599716b5518fc9b7564d5
OVO <OVO@DESKTOP-80T0MHT>
2020-07-07 11:07:38 +0800

    添加项目文件。

---
+++ Grpc.ClientTest/Controlers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Grpc.Net.ClientFactory;
+using Grpc.ServiceTest;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Grpc.ClientTest.Controlers
+{
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            dass();
+            return Ok(ViewBag.name);
+        }
+
+        private void dass()
+        {
+            //using var channel = GrpcChannel.ForAddress("https://localhost:5020");
+            var channel = GrpcChannel.ForAddress("http://localhost:5020", new GrpcChannelOptions
+            {
+                HttpClient = new HttpClient(new HttpClientHandler() { ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator })
+            });
+            var client = new Greeter.GreeterClient(channel);
+            var response = client.SayHello(new HelloRequest { Name = "gRPCWeb 测试" });
+            ViewBag.name = response.Message;
+        }
+    }
+}
